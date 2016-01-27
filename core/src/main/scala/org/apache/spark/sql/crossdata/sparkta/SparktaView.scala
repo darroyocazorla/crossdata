cae1af9ebbe09b31c31f03ae29d5744f06cdc34e
darroyocazorla <darroyo@stratio.com>
2016-01-27 13:16:35 +0100

    get schema from both the cube and the output

--- core/src/main/scala/org/apache/spark/sql/crossdata/sparkta/SparktaView.scala
+++ core/src/main/scala/org/apache/spark/sql/crossdata/sparkta/SparktaView.scala
@@ -9,8 +9,20 @@ case class SparktaView(cube: Cube, sparktaOutput: Seq[SparktaOutput]){
 
   require(sparktaOutput.nonEmpty, "At least one output is required")
   def simpleString: String = ???
-  def schema: StructType =
+
+  def cubeSchema: StructType =
     sparktaOutput.head.schema
+
+  def schema: StructType = {
+    val nameToField = cubeSchema.fields.map(f => f.name -> f).toMap
+    // It filters out aggregate columns from the cube
+    StructType(
+      cube.groupByExprs.flatMap {
+        case ColumnIdentifier(colName) => nameToField.get(colName)
+      }
+    )
+  }
+
 }
 
 object OutputDatasource extends Enumeration{
@@ -21,9 +33,11 @@ object OutputDatasource extends Enumeration{
   val Parquet = Value("parquet")
   val CSV = Value("com.databricks.spark.csv")
   val Solr = Value("solr") //Lucid-words datasource
+  val JDBC = Value("jdbc")
+  val JSON = Value("json")
 
 }
-import OutputDatasource._
+import org.apache.spark.sql.crossdata.sparkta.OutputDatasource._
 // TODO serialize schema to json or String
 // TODO options => options should contain datasource specific info => how to manage it?
 case class SparktaOutput(datasource: OutputDatasource, schema: StructType, options: Map[String, String] = Map.empty)
--- core/src/main/scala/org/apache/spark/sql/crossdata/sparkta/aggregationExpression.scala
+++ core/src/main/scala/org/apache/spark/sql/crossdata/sparkta/aggregationExpression.scala
@@ -13,4 +13,5 @@ case class ColumnOperation(alias: String, field: ColumnIdentifier, opType: Opera
 
 case class GlobalCountOperation(alias: String, isDistinct: Boolean = false) extends AggregationExpression
 
+// TODO According to Sparkta doc, the colIdentifier can have name and field, so an alias should be added
 case class ColumnIdentifier(field: String)
